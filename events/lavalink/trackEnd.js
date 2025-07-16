54d02fec42696dc3c48b47d83c18a04b858d0c75
golddog068 <turnerelijah68@gmail.com>
2025-07-16 16:47:30 -0400

    Delete events/lavalink directory

--- events/lavalink/trackEnd.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  name: 'trackEnd',
-  async execute(client, player, track) {
-    const channel = client.channels.cache.get(player.textChannelId);
-    if (player.collector) {
-      player.collector.stop();
-    }
-  },
-};
