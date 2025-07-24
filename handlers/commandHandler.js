81891d3346b882e49edf6d4d61d0e2cbf0b286d3
golddog068 <turnerelijah68@gmail.com>
2025-07-24 14:30:53 -0400

    Update commandHandler.js

--- handlers/commandHandler.js
+++ handlers/commandHandler.js
@@ -1,26 +1,14 @@
 const fs = require('fs');
 const path = require('path');
+
 module.exports = (client) => {
   client.commands = new Map();
 
   const commandsPath = path.join(__dirname, '../commands');
-  fs.readdirSync(commandsPath).forEach((category) => {
-    const categoryPath = path.join(commandsPath, category);
-    const commandFiles = fs
-      .readdirSync(categoryPath)
-      .filter((file) => file.endsWith('.js'));
-
-    for (const file of commandFiles) {
-      const command = require(path.join(categoryPath, file));
-
-      client.commands.set(command.data.name, { ...command, category });
-    }
-  });
-  let commandCount = 0;
-  let categoryCount = 0;
-
   const categories = fs.readdirSync(commandsPath);
-  categoryCount = categories.length;
+
+  let commandCount = 0;
+  let categoryCount = categories.length;
 
   categories.forEach((category) => {
     const categoryPath = path.join(commandsPath, category);
@@ -30,8 +18,13 @@ module.exports = (client) => {
 
     for (const file of commandFiles) {
       const command = require(path.join(categoryPath, file));
-      client.commands.set(command.data.name, { ...command, category });
-      commandCount++;
+
+      if (command?.data?.name && typeof command.execute === 'function') {
+        client.commands.set(command.data.name, { ...command, category });
+        commandCount++;
+      } else {
+        console.warn(`⚠️ Skipping invalid command file: ${file}`);
+      }
     }
   });
 
